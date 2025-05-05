ce66dc1c64e3fb6ca041756610013af768ea6821
hitalo <hitalo.2014.hs@gmail.com>
2025-05-05 07:13:17 -0300

    refactor: removed ingredients router from routes

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -2,11 +2,9 @@ import { Express } from "express";
 import { usersRouters } from "./users.routes";
 import { sessionRouters } from "./session.routes";
 import { recipeRoutes } from "./recipes.routes";
-import { ingredientsRoutes } from "./ingredients.routes";
 
 export const appRoutes = (app: Express) => {
   app.use("/users", usersRouters());
   app.use("/session", sessionRouters());
   app.use("/recipes", recipeRoutes());
-  app.use("/ingredients", ingredientsRoutes());
 };
